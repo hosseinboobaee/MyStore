4f0c362c3df2fa9c7816639e8baf923b349f1c75
hosein boobaee <m.hossein.boobaee@gmail.com>
2024-08-05 16:03:15 +0330

    14030515

--- Core/DTOs/Paging/BasePaging.cs
+++ Core/DTOs/Paging/BasePaging.cs
@@ -1,26 +1,31 @@
-﻿namespace Core.DTOs.Paging
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core.DTOs.Paging
 {
-    public class BasePaging
+    public class PageList<T>: List<T>
     {
-        public BasePaging()
+        public PageList(IEnumerable<T> items, int pageNumber, int pageSize, int count)
         {
-            PageId = 1;
-            TakeEntity = 10;
+            this.TotalPage = (int)Math.Ceiling(count / (double)pageSize); //20/10=2
+            this.CurrentPage = pageNumber;
+            this.PageSize = pageSize;
+            this.TotalCount = count;
+            this.AddRange(items);
         }
 
 
-        public int PageId { get; set; }
+        public int CurrentPage { get; set; }
 
-        public int PageCount { get; set; }
+        public int TotalPage { get; set; }
 
-        public int ActivePage { get; set; }
+        public int PageSize { get; set; }
 
-        public int StartPage { get; set; }
-
-        public int EndPage { get; set; }
-
-        public int TakeEntity { get; set; }
-
-        public int SkipEntity { get; set; }
+        public int TotalCount { get; set; }
+        public static async Task<PageList<T>> CreateAsync(IQueryable<T> sourece, int pageNumber, int pageSize)
+        {
+            var count = await sourece.CountAsync();
+            var items = await sourece.Skip((pageNumber-1 ) * pageSize).Take(pageSize).ToListAsync();
+            return new PageList<T>(items,pageNumber,pageSize,count);
+        }
     }
 }
